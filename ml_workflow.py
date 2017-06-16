94e4a50bc8c43ce8e1f91f09dd17d65fa624ae28
Pietro Mattia Campi <pietro.campi.dev@gmail.com>
2017-06-16 09:56:51 +0200

    Added model evaluation

--- ml_workflow.py
+++ ml_workflow.py
@@ -49,9 +49,13 @@ def train_model(sentences: List[List[str]], num_features=300,
     model: model
         trained word2vec model
     """
-    print("Started training.")
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
-                        level=logging.INFO)
+    info = "Started training with {} features, {} minimum word count, ".format(
+        num_features, min_word_count)
+    info2 = "{} threads, {} context window, {} downsampling.".format(
+        num_workers, context, downsampling)
+    print(info + info2)
+    # logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+    #                     level=logging.INFO)
 
     start_time = time.time()
     model = word2vec.Word2Vec(sentences, workers=num_workers,
@@ -63,7 +67,7 @@ def train_model(sentences: List[List[str]], num_features=300,
     return model
 
 
-def complete_workflow(save=True, keep_stopwords=True, lemmatize=False):
+def complete_workflow(save=True, keep_stopwords=False, lemmatize=True):
     """A convenience function to run all the workflow."""
     filepath = "train_data/Stephen Chbosky - Ragazzo da parete.txt"
     sentences = preprocess_file(filepath, keep_stopwords, lemmatize)
@@ -81,21 +85,94 @@ def complete_workflow(save=True, keep_stopwords=True, lemmatize=False):
                         downsampling=downsampling)
 
     if save:
-        model_name = "{}feat_{}minwords_{}context".\
-                     format(features, word_count, context)
-        if lemmatize:
-            model_name = model_name + "_lemmatized"
-        if keep_stopwords:
-            model_name = model_name + "_with_stopwords"
-        else:
-            model_name = model_name + "_no_stopwords"
+        save_model(model, features, word_count, context,
+                   lemmatize, keep_stopwords)
 
-        model.save(model_name)
-        print("Model saved! Workflow completed.")
-    else:
-        print("Workflow completed.")
-
+    print("Workflow completed.")
     return model
+
+
+def multiple_training():
+    """Batch training function."""
+    filepath = "train_data/Stephen Chbosky - Ragazzo da parete.txt"
+    keep_stopwords = False
+    lemmatize = True
+    sentences = preprocess_file(filepath, keep_stopwords, lemmatize)
+
+    features = 300
+    word_count = [2, 5, 10]
+    workers = 4
+    context = [3, 5]
+    downsampling = 1e-3
+
+    models = [[train_model(sentences, features, wc,
+                           workers, ctx, downsampling)
+               for wc in word_count]
+              for ctx in context]
+
+    return models, word_count, context
+
+
+def save_model(model, num_features, min_word_count, context, lemmatized,
+               keep_stopwords=False):
+    """Save a word2vec model."""
+    model_name = "saved_models/{}feat_{}minwords_{}context"\
+                 .format(num_features, min_word_count, context)
+
+    if lemmatized:
+        model_name = model_name + "_lemmatized"
+
+    if keep_stopwords:
+        model_name = model_name + "_with_stopwords"
+    else:
+        model_name = model_name + "_no_stopwords"
+
+    model.save(model_name)
+    print("Saved model " + model_name)
+
+
+def evaluate_model(model, n=50):
+    """Evaluate the model with the target emotions.
+
+    Parameters
+    ----------
+    model: gensim.Word2Vec
+        the trained model to evaluate
+
+    n: int
+        the top n most similar words to ask for
+
+    Returns
+    -------
+    goodness: float
+        measure of good the model is
+    """
+    def measure_goodness(similar_words):
+        """Give a goodness measure of similar_words."""
+        fraction = n // 5
+        neg = 0
+
+        for i in range(len(similar_words)):
+            word, similarity = similar_words[i]
+            if i >= fraction and similarity >= 0.99:
+                neg = neg + 1
+
+        return neg
+
+    emotions = ["rabbia", "anticipazione", "paura", "disgusto", "gioia",
+                "tristezza", "fiducia", "sorpresa"]
+
+    result = {}
+    negatives = []
+    for key in emotions:
+        try:
+            similar_words = model.most_similar(positive=[key], topn=n)
+            negatives.append(measure_goodness(similar_words))
+            result[key] = similar_words
+        except KeyError:
+            print("{} not in vocabulary".format(key))
+
+    return result, negatives
 
 
 if __name__ == "__main__":
